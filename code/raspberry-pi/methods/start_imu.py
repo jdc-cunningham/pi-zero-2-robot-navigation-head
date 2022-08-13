3fc57a492ae7e9aa13ac1a13c0afd79ec0e91afa
Jacob Cunningham <jcun4128@gmail.com>
2022-08-13 15:27:30 +0100

    crude imu check

--- code/raspberry-pi/methods/start_imu.py
+++ code/raspberry-pi/methods/start_imu.py
@@ -1,6 +1,9 @@
 # https://stackoverflow.com/questions/45951964/pyinstaller-is-not-recognized-as-internal-or-external-command
 # https://stackoverflow.com/questions/41171791/how-to-suppress-or-capture-the-output-of-subprocess-run
 # https://stackoverflow.com/questions/7152340/using-a-python-subprocess-call-to-invoke-a-python-script
+# https://stackoverflow.com/questions/8713983/python-catching-the-output-from-subprocess-call-with-stdout
+# https://stackoverflow.com/questions/48875596/python-run-python-script-as-subprocess-and-get-the-output (works)
+# https://stackoverflow.com/questions/24849998/how-to-catch-exception-output-from-python-subprocess-check-output
 
 import subprocess
 from subprocess import Popen, PIPE
@@ -13,10 +16,12 @@ def imu_on_bus():
   return len(bus_1_scan.stderr) == 0 and bus_1_scan.stdout.find("68") > -1
 
 def sample_imu():
-  print("pre sample")
-  imu_sample = subprocess.Popen(["python3", "mpu9250.py"])
-  print("post sample")
-  print(imu_sample.communcate())
+  imu_sample = ""
+
+  try:
+    return len(subprocess.check_output(["python3", "mpu9250_single.py"]).decode('utf-8').rstrip()) > 10 # 10 is a random number but long enough
+  except subprocess.CalledProcessError as e:
+    return False # not good
 
 def wake_imu():
   global imu_awake, imu_found
@@ -32,7 +37,16 @@ def wake_imu():
       print("failed to find imu")
       break
 
-  if (imu_found):
-    sample_imu()
+  check_count = 0
 
-wake_imu()
+  if (imu_found):
+    while (not imu_awake):
+      imu_awake = sample_imu()
+      check_count += 1
+
+      if (check_count == max_check and not imu_awake):
+        return False
+      else:
+        return True
+
+print(wake_imu())
