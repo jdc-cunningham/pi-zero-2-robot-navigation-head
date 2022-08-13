9ab8eb945d7ae3c8533e251b6c46ad0dfb15c91c
Jacob Cunningham <jcun4128@gmail.com>
2022-08-13 05:48:30 +0100

    push this up

--- code/raspberry-pi/methods/start_imu.py
+++ code/raspberry-pi/methods/start_imu.py
@@ -1,13 +1,38 @@
-import subprocess
+# https://stackoverflow.com/questions/45951964/pyinstaller-is-not-recognized-as-internal-or-external-command
+# https://stackoverflow.com/questions/41171791/how-to-suppress-or-capture-the-output-of-subprocess-run
+# https://stackoverflow.com/questions/7152340/using-a-python-subprocess-call-to-invoke-a-python-script
 
+import subprocess
+from subprocess import Popen, PIPE
+
+imu_found = False
 imu_awake = False
 
-def scan_bus_for_imu():
-  subprocess.run(["i2cdetect", "-y", "1"])
+def imu_on_bus():
+  bus_1_scan = subprocess.run(["i2cdetect", "-y", "1"], capture_output=True, text=True)
+  return len(bus_1_scan.stderr) == 0 and bus_1_scan.stdout.find("68") > -1
 
-# def wake_imu():
-#   global imu_awake
-#   while (not imu_awake):
-#     subprocess.run([])
+def sample_imu():
+  print("pre sample")
+  imu_sample = subprocess.Popen(["python3", "mpu9250.py"])
+  print("post sample")
+  print(imu_sample.communcate())
 
-scan_bus_for_imu()
+def wake_imu():
+  global imu_awake, imu_found
+
+  max_check = 5
+  check_count = 0
+
+  while (not imu_found):
+    imu_found = imu_on_bus()
+    check_count += 1
+    
+    if (not imu_found and check_count == max_check):
+      print("failed to find imu")
+      break
+
+  if (imu_found):
+    sample_imu()
+
+wake_imu()
