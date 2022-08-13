82f6f4a010b059bd67533633d8bbea3d94f99622
Jacob Cunningham <jcun4128@gmail.com>
2022-08-13 10:11:11 -0500

    try this

--- code/raspberry-pi/methods/servo.py
+++ code/raspberry-pi/methods/servo.py
@@ -1,6 +1,7 @@
 # https://www.teachmemicro.com/raspberry-pi-pwm-servo-tutorial/
 
 import pigpio
+import time
 
 pi = pigpio.pi()
 
@@ -13,3 +14,13 @@ tilt_servo = 13
 def boot_center():
   pi.set_servo_pulsewidth(pan_servo, 1440) # > 1500, left
   pi.set_servo_pulsewidth(tilt_servo, 1090) # < 1500 up
+
+def tmp_turn_left_right():
+  for pw in range(1440,1540,1): # 0 to 100 duty cycle range, third param is step
+    servo.ChangeDutyCycle(pw)
+    time.sleep(0.1)
+
+tmp_turn_left_right()
+
+# def tmp_look_around():
+
