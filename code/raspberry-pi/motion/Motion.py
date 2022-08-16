ec14a8ff55e20a2a8e865b95f6bffe027c9c344d
Jacob Cunningham <jcun4128@gmail.com>
2022-08-15 20:25:26 -0500

    fill in the blocks

--- code/raspberry-pi/motion/Motion.py
+++ code/raspberry-pi/motion/Motion.py
@@ -1,16 +1,35 @@
+import pigpio
+import time
+import asyncio
+import websockets
+
+# there is a boot script that centers the servos
+
 class Motion:
   def __init__(self):
-    self.name = "UwU Navu"
+    self.pi = pigpio.pi()
+    self.pan_servo = 12
+    self.tilt_servo = 13
 
   class PanTilt():
     def __init__(self):
       self.pan_servo
       self.tilt_servo
+
+  # this is a websocket control against the WiFi buggy
   class Wheels():
     def __init__(self):
+      self.socket_addr = "ws://192.168.1.195:80"
       self.left
       self.right
       self.move_forward
       self.move_left
       self.move_right
       self.move_backward
+
+    async def send_command(self, msg):
+      async with websockets.connect(self.socket_addr) as websocket:
+        await websocket.send(msg)
+
+    # hmm 
+    # asyncio.run(send_command())
--- code/raspberry-pi/sensors/Sensors.py
+++ code/raspberry-pi/sensors/Sensors.py
@@ -1,3 +1,15 @@
+import VL53L0X
+import RPi.GPIO as GPIO
+import time
+import subprocess
+import sys
+import tfmplus as tfmP   # Import the `tfmplus` module v0.1.0
+from tfmplus import *    # and command and paramter defintions
+
+from os.path import exists
+from mpu9250_jmdev.registers import *
+from mpu9250_jmdev.mpu_9250 import MPU9250
+
 class Sensors:
   def __init__(self):
     self.camera = self.Camera()
@@ -8,6 +20,17 @@ class Sensors:
   class Camera():
     def __init__(self):
       self.name = "Raspberry Pi Camera Module V2 8MP"
+      self.led_pin = 6 # this is used in low light
+
+      GPIO.setmode(GPIO.BCM)
+      GPIO.setwarnings(False)
+      GPIO.setup(6, GPIO.OUT)
+
+    def led_on(self):
+      GPIO.output(self.led_pin, GPIO.HIGH)
+
+    def led_off(self):
+      GPIO.output(self.led_pin, GPIO.LOW)
 
     def take_photo(self):
       filename = str(int(time.time())) + ".jpg"
@@ -18,11 +41,59 @@ class Sensors:
   class Tof():
     def __init__(self):
       self.name ="Pololu VL53L0X"
+      self.tof = VL53L0X.VL53L0X()
+
+    def get_distance(self):
+      self.tof.start_ranging(VL53L0X.VL53L0X_BETTER_ACCURACY_MODE)
+      distance = self.tof.get_distance()
+      self.tof.stop_ranging()
+      return (distance / 10) * 0.39 # cm to in (1 -> 0.393701)
 
   class Lidar():
     def __init__(self):
       self.name = "TFmini-s"
+      self.serial_port = "/dev/serial0" # Raspberry Pi normal serial port
+      self.serial_rate = 115200 # TFMini-Plus default baud
+      self.lidar = tfmP
+      
+    def get_distanec(self):
+      lidar_data = self.lidar.getData()
+      return lidar_data.dist * 0.39
 
+  # I think this will always be running as a thread
+  # if not store data in memory I guess during a motion
   class Imu():
     def __init__(self):
       self.name = "MPU9250"
+      self.measurements = []
+      self.mpu = MPU9250(
+        address_ak=AK8963_ADDRESS,
+        address_mpu_master=MPU9050_ADDRESS_68, # In 0x68 Address
+        address_mpu_slave=None,
+        bus=1,
+        gfs=GFS_1000,
+        afs=AFS_8G,
+        mfs=AK8963_BIT_16,
+        mode=AK8963_MODE_C100HZ
+      )
+
+      self.mpu.configure() # Apply the settings to the registers.
+
+    def get_all(self):
+      return [
+        self.mpu.readAccelerometerMaster(),
+        self.mpu.readGyroscopeMaster(),
+        self.mpu.readMagnetometerMaster()
+      ]
+
+    
+    def get_single(self, type = 0): # 0, 1, 2, 3 accel, gyro, mag
+      if (type == 0):
+        return self.mpu.readAccelerometerMaster()
+      elif (type == 1):
+        return self.mpu.readGyroscopeMaster()
+      else:
+        return self.mpu.readMagnetometerMaster()
+
+    def start_sampling(self):
+      self.measurements.append() # have to think about this
