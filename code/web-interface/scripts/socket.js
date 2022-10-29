7e45553bfb93e5b2776d8da31c8f7d3e44d53b83
Jacob Cunningham <jcun4128@gmail.com>
2022-10-29 12:00:53 -0500

    minor changes, need to work out of pi

--- code/web-interface/scripts/socket.js
+++ code/web-interface/scripts/socket.js
@@ -1,8 +1,11 @@
 let socketInterval = null;
 let socket = null;
+let wasConnected = false;
+
+const socketStatus = document.getElementById('socket-status');
 
 const connectToNavigationDevice = () => {
-  socket = new WebSocket('ws://192.168.1.155:5678'); // esp01 on robot
+  socket = new WebSocket('ws://192.168.1.228:5678'); // esp01 on robot
 
   // connection opened, send messages to robot
   socket.addEventListener('open', function (event) {
@@ -14,16 +17,25 @@ const connectToNavigationDevice = () => {
     }, 1000);
   });
  
-// listen for messages from floating navigation sensor assembly
+  // listen for messages from floating navigation sensor assembly
   socket.addEventListener('message', function (event) {
     const msg = event.data;
     console.log(msg);
   });
  
   socket.addEventListener('close', function (event) {
-    socketStatus.innerText = 'connection lost';
+    socketStatus.innerText = wasConnected ? 'connection lost, reconnecting...' : 'failed to connect, connecting...';
     clearInterval(socketInterval);
- });
+  });
+
+  socket.addEventListener('error', function (event) {
+    socketStatus.innerText = 'error connecting, trying again...';
+    clearInterval(socketInterval);
+
+    setTimeout(() => {
+      connectToNavigationDevice();
+    }, 1000);
+  });
 }
 
 connectToNavigationDevice();
