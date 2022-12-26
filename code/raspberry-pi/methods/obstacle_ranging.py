49ec9cca5af45477de408f72ebe1b093f24a26c5
Jacob Cunningham <jcun4128@gmail.com>
2022-12-26 17:21:13 -0600

    try this

--- code/raspberry-pi/methods/obstacle_ranging.py
+++ code/raspberry-pi/methods/obstacle_ranging.py
@@ -1,7 +1,14 @@
+import time
 sys.path.append('/home/pi/chisel-bot-2/code/raspberry-pi')
 
 # from methods.start_imu import *
 from sensors.Sensors import *
-from servo import pan_control, tilt_control
+from servo import boot_center, look_right
 
 sensors = Sensors()
+
+time.sleep(2)
+boot_center()
+look_right(21)
+
+print('distance to box: ' + sensors.tof.get_distance() + ', ' + sensors.lidar.get_distance())
