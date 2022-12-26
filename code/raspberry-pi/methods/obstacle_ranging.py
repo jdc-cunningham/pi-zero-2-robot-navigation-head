b8f357420fcdd15c2e848e02bbfbca83c4bbff84
Jacob Cunningham <jcun4128@gmail.com>
2022-12-26 17:24:26 -0600

    missing

--- code/raspberry-pi/methods/obstacle_ranging.py
+++ code/raspberry-pi/methods/obstacle_ranging.py
@@ -1,3 +1,4 @@
+import sys
 import time
 sys.path.append('/home/pi/chisel-bot-2/code/raspberry-pi')
 
@@ -9,6 +10,6 @@ sensors = Sensors()
 
 time.sleep(2)
 boot_center()
-look_right(21)
+look_right(21) # know it's 21 deg to the right
 
 print('distance to box: ' + sensors.tof.get_distance() + ', ' + sensors.lidar.get_distance())
