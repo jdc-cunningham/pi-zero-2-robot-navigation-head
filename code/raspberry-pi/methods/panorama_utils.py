62129610391262fa2473b5f1ddf3ecac221cb035
Jacob Cunningham <jcun4128@gmail.com>
2023-01-12 17:57:57 -0600

    find min max red

--- code/raspberry-pi/methods/panorama_utils.py
+++ code/raspberry-pi/methods/panorama_utils.py
@@ -25,10 +25,30 @@ def iterate_samples():
 
   print(width, height)
 
+  smallest_r = 255
+  smallest_g = 255
+  smallest_b = 255
+
+  largest_r = 0
+  largest_g = 0
+  largest_b = 0
+
   for x in range(0, width, 1):
     for y in range(0, height, 1):
       # output to array for JS html/css vis conf of colors
       # print(",".join(str(rgb_sliver_sample[y, x]).split("  ")) + ",")
-      print(",".join(str(rgb_center_sample[y, x]).split("  ")) + ",")
+      # print(",".join(str(rgb_center_sample[y, x]).split("  ")) + ",")
+      r, g, b = rgb_center_sample[y, x]
+
+      smallest_r = r if r < smallest_r else smallest_r
+      smallest_g = g if g < smallest_g else smallest_g
+      smallest_b = b if b < smallest_b else smallest_b
+
+      largest_r = r if r > largest_r else largest_r
+      largest_g = g if g > largest_g else largest_g
+      largest_b = b if b > largest_b else largest_b
+
+  print(smallest_r, smallest_g, smallest_b)
+  print(largest_r, largest_g, largest_b)
 
 iterate_samples()
