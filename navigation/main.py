65742a1a32c5cc9a81bd5c9642ea96304b5b5f77
Jacob Cunningham <jcun4128@gmail.com>
2025-01-11 23:38:29 -0600

    need other folders

--- navigation/main.py
+++ navigation/main.py
@@ -2,7 +2,7 @@ from servo.servo import Servo
 import tfmplus.tfmplus as tfmP
 from tfmplus.tfmplus import *
 from narrow_angle_sensor.sensor import NarrowSensor
-from VL53L0X import VL53L0X
+from VL53L0X.python import VL53L0X
 from wide_angle_sensor.sensor import WideSensor
 
 # servo = Servo()
