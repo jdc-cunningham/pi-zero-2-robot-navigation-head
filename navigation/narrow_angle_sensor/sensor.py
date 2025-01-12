375188b154cae57bd215d67a575456ca84f6fa61
Jacob Cunningham <jcun4128@gmail.com>
2025-01-11 22:55:28 -0600

    bring in sensor files and add basic code

---
+++ navigation/narrow_angle_sensor/sensor.py
@@ -0,0 +1,23 @@
+import tfmplus as tfmP
+from tfmplus import *
+import time
+
+class Sensor():
+  def __int__(self):
+    self.name = "TFmini-s"
+    self.serial_port = "/dev/serial0" # Raspberry Pi normal serial port
+    self.serial_rate = 115200 # TFMini-Plus default baud
+    self.sensor = tfmP
+
+    self.setup()
+
+  def setup(self):
+    self.sensor.begin(self.serial_port, self.serial_rate)
+    self.sensor.sendCommand( SOFT_RESET, 0)
+    time.sleep(0.5)
+    self.sensor.sendCommand(SET_FRAME_RATE,FRAME_20)
+    time.sleep(0.5) # 1 second lag per scan
+
+  def get_distance(self):
+    self.sensor.getData()
+    return self.lidar.dist * 0.39
---
+++ navigation/wide_angle_sensor/sensor.py
@@ -0,0 +1,12 @@
+from python import VL53L0X
+
+class Sensor():
+  def __init__(self):
+    self.sensor = VL53L0X.VL53L0X()
+
+  def get_distance(self):
+    self.sensor.start_ranging(VL53L0X.VL53L0X_BETTER_ACCURACY_MODE)
+    self.sensor.start_ranging(VL53L0X.VL53L0X_BETTER_ACCURACY_MODE)
+    distance = self.sensor.get_distance()
+    self.sensor.stop_ranging()
+    return (distance / 10) * 0.39 # cm to in (1 -> 0.393701)
