0145c65eb3819d92ab3c56e0c0d82f07e400b452
Jacob Cunningham <jcun4128@gmail.com>
2025-01-12 04:20:55 +0000

    set servo boot centers

---
+++ navigation/main.py
@@ -0,0 +1,5 @@
+from servo.servo import Servo
+
+servo = Servo()
+servo.boot_center()
+
--- navigation/servo/servo.py
+++ navigation/servo/servo.py
@@ -5,10 +5,10 @@ class Servo:
     self.pigpio = pigpio.pi()
     self.pan_servo_pin = 12
     self.pan_servo_pos = 0
-    self.pan_servo_center_pos = 0  # 1500 midpoint, 2500 max
+    self.pan_servo_center_pos = 1450  # 1500 midpoint, 2500 max
     self.tilt_servo_pin = 13
     self.tilt_servo_pos = 0
-    self.tilt_servo_center_pos = 0
+    self.tilt_servo_center_pos = 1200
 
   def boot_center(self):
     self.pigpio.set_servo_pulsewidth(self.tilt_servo_pin, self.tilt_servo_center_pos)
