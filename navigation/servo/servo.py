fe5d0f9230bb5d2eda63af0e61aa1863b05943d3
Jacob Cunningham <jcun4128@gmail.com>
2025-01-11 22:45:32 -0600

    set default pos based on startup center

--- navigation/servo/servo.py
+++ navigation/servo/servo.py
@@ -4,10 +4,10 @@ class Servo:
   def __init__(self):
     self.pigpio = pigpio.pi()
     self.pan_servo_pin = 12
-    self.pan_servo_pos = 0
+    self.pan_servo_pos = 1200
     self.pan_servo_center_pos = 1450  # 1500 midpoint, 2500 max
     self.tilt_servo_pin = 13
-    self.tilt_servo_pos = 0
+    self.tilt_servo_pos = 1200
     self.tilt_servo_center_pos = 1200
 
   def boot_center(self):
