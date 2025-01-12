da9a064d616f98f0eb67c853f5901c9d33c17159
Jacob Cunningham <jcun4128@gmail.com>
2025-01-11 22:06:23 -0600

    folder scaffold, start servo

---
+++ navigation/servo/servo.py
@@ -0,0 +1,15 @@
+import pigpio
+
+class Servo:
+  def __init__(self):
+    self.pigpio = pigpio.pi()
+    self.pan_servo_pin = 12
+    self.pan_servo_pos = 0
+    self.pan_servo_center_pos = 0  # 1500 midpoint, 2500 max
+    self.tilt_servo_pin = 13
+    self.tilt_servo_pos = 0
+    self.tilt_servo_center_pos = 0
+
+  def boot_center(self):
+    self.pigpio.set_servo_pulsewidth(self.tilt_servo_pin, self.tilt_servo_center_pos)
+    self.pigpio.set_servo_pulsewidth(self.pan_servo_pin, self.pan_servo_center_pos)
