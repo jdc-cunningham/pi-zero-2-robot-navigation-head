69070ececc94f4630dc201882fe7be87fd9e1e8e
Jacob Cunningham <jcun4128@gmail.com>
2025-01-12 22:19:36 -0600

    programmed turn

--- vehicle/seeeduino/motion.h
+++ vehicle/seeeduino/motion.h
@@ -3,14 +3,14 @@
 // continuous rotation servos
 struct servo {
   int pin;
-  int stopPos;  // degrees
-  int speedPos; // degrees
-  int runTime;  // seconds
-  Servo servo;  // seems silly, was looking for a self/this equivalent
+  int stopPos;     // degrees
+  int forwardPos;  // degrees
+  int backwardPos; // degrees
+  Servo servo;     // seems silly, was looking for a self/this equivalent
 };
 
-servo leftServo = servo{0, 90, 85, 1};
-servo rightServo = servo{3, 90, 97, 1};
+servo leftServo = servo{0, 90, 85, 95};
+servo rightServo = servo{3, 90, 97, 83};
 
 bool motionInProgress = false;
 
@@ -32,15 +32,70 @@ void moveForward(int inches)
 {
   if (motionInProgress) return;
 
-    leftServo.servo.write(leftServo.speedPos);
-    rightServo.servo.write(rightServo.speedPos);
-    delay(150);
-    stopMoving();
+  leftServo.servo.write(leftServo.forwardPos);
+  rightServo.servo.write(rightServo.forwardPos);
+  delay(800); // 1700 10 inch
+  stopMoving();
+}
+
+void moveBackward(int inches)
+{
+  if (motionInProgress) return;
+
+  leftServo.servo.write(leftServo.forwardPos);
+  rightServo.servo.write(rightServo.forwardPos);
+  delay(800); // 1700 10 inch
+  stopMoving();
+}
+
+void turnLeft(int degrees)
+{
+  if (motionInProgress) return;
+
+  leftServo.servo.write(leftServo.backwardPos);
+  rightServo.servo.write(rightServo.forwardPos);
+  delay(800);
+  stopMoving();
+}
+
+void turnRight(int degrees)
+{
+  if (motionInProgress) return;
+
+  leftServo.servo.write(leftServo.forwardPos);
+  rightServo.servo.write(rightServo.backwardPos);
+  delay(800);
+  stopMoving();
+}
+
+// advanced preformed string
+// rc_ls_90_10_rs_90_10
+// translates to: raw command, left servo 90 deg 10 ms, rs 90 deg 10 ms
+void rawCommand(String command)
+{
+  // not how this will work just putting this in here for video demo
+  moveForward(1);
+  moveForward(1);
+  turnRight(1);
+  moveForward(1);
+  moveForward(1);
+  turnRight(1);
+  moveForward(1);
+  moveForward(1);
+  turnRight(1);
+  moveForward(1);
+  moveForward(1);
+  turnRight(1);
 }
 
 // ex. mf_010 for move forward 10 inches
-void parseMotionCommand(String motionComand)
+void parseMotionCommand(String motionCommand)
 {
+  if (motionCommand.indexOf("rc_") == 0)
+  {
+    rawCommand(motionCommand);
+  }
+
   if (motionCommand.indexOf("mf_") == 0)
   {
     int inches = motionCommand.substring(3, 6).toInt();
@@ -48,8 +103,24 @@ void parseMotionCommand(String motionComand)
     motionCommand = "";
   }
 
-  if (motionCommand.indexOf("mb_"))
+  if (motionCommand.indexOf("mb_") == 0)
   {
-  
+    int inches = motionCommand.substring(3, 6).toInt();
+    moveBackward(inches);
+    motionCommand = "";
+  }
+
+  if (motionCommand.indexOf("tr_") == 0)
+  {
+    int inches = motionCommand.substring(3, 6).toInt();
+    turnRight(inches);
+    motionCommand = "";
+  }
+
+  if (motionCommand.indexOf("tl_") == 0)
+  {
+    int inches = motionCommand.substring(3, 6).toInt();
+    turnLeft(inches);
+    motionCommand = "";
   }
 }
