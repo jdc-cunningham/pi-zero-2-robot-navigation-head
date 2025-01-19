d4a2388235a555406bee83f2f40b42f079923ad6
Jacob Cunningham <jcun4128@gmail.com>
2025-01-19 16:21:33 -0600

    start manually setting angles for floor sweep

--- raspberry-pi/main.py
+++ raspberry-pi/main.py
@@ -2,4 +2,9 @@ from servo.servo import Servo
 from narrow_angle_sensor.sensor import NarrowSensor
 from wide_angle_sensor.sensor import WideSensor
 from led.led import Led
+from imu_sensor.imu_sensor import IMUSensor
+
+motion = Servo()
+
+
 
