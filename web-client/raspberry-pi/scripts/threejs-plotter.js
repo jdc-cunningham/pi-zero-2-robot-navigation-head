52ea7365d1e5d9a523c5e5aeca6f13fa7698542c
Jacob Cunningham <jcun4128@gmail.com>
2025-01-19 21:12:42 -0600

    working plot

--- web-client/raspberry-pi/scripts/threejs-plotter.js
+++ web-client/raspberry-pi/scripts/threejs-plotter.js
@@ -1,9 +1,18 @@
+// this has to be hosted somewhere, in my case a Raspberry Pi Apache Web Server
+// needs CORS
+const baseGlbPath = "http://192.168.1.144/3d-files/";
+const robotGlbFilename = "basic-robot-model.glb";
+const loader = new THREE.GLTFLoader();
+const gltfs = {};
+const scene = new THREE.Scene();
+
+const degToRad = (deg) => deg * 0.0174533;
+
 const threejsPlotChart = () => {
   // https://threejs.org/docs/#manual/en/introduction/Creating-a-scene
   THREE.Object3D.DefaultUp.set(0, 0, 1); // set Z as vertical axes
-  const scene = new THREE.Scene();
   // 10 z distanece
-  const camera = new THREE.PerspectiveCamera(100, window.innerWidth / window.innerHeight, 0.1, 1000);
+  const camera = new THREE.PerspectiveCamera(70, window.innerWidth / window.innerHeight, 0.1, 2000);
   const canvas = document.getElementById("threejs-canvas");
   const renderer = new THREE.WebGLRenderer({ canvas: canvas }); // https://stackoverflow.com/a/21646450/2710227
   const canvasParent = document.querySelector('.app__left-plot');
@@ -12,7 +21,7 @@ const threejsPlotChart = () => {
 
   renderer.setSize(canvasParent.offsetWidth, canvasParent.offsetWidth); // add false for lower resolution after dividing x/y values
   // renderer.setPixelRatio(window.devicePixelRatio);
-  renderer.setPixelRatio(2); // looks great
+  // renderer.setPixelRatio(2); // looks great
   // https://discourse.threejs.org/t/render-looks-blurry-and-pixelated-even-with-antialias-true-why/12381/5
 
   // add orbit controls
@@ -26,12 +35,13 @@ const threejsPlotChart = () => {
   scene.add(axesHelper);
 
   // add grid overlay
-  const size = 100;
-  const divisions = 10;
+  const size = 5;
+  const divisions = 20;
   const gridHelper = new THREE.GridHelper(size, divisions);
-  // const zVector = new THREE.Vector3(0, 0, 1);
+  const zVector = new THREE.Vector3(0, 0, 1);
+  const yVector = new THREE.Vector3(0, 1, 0);
   gridHelper.rotateX(Math.PI / 2); // https://stackoverflow.com/a/58554774/2710227
-  // gridHelper.lookAt(zVector);
+  gridHelper.lookAt(yVector);
   scene.add(gridHelper);
 
   controls.update();
@@ -44,8 +54,8 @@ const threejsPlotChart = () => {
   }
 
   // camera
-  camera.position.set( 0, 0, 100 );
-  camera.lookAt( 0, 0, 0 );
+  camera.position.set( 0, 0, 1 );
+  // camera.lookAt( 0, 0, 0 );
 
   // line material
   let material = new THREE.LineBasicMaterial({ color: 0x00FF00 });
@@ -85,6 +95,47 @@ const threejsPlotChart = () => {
   }
 
   renderer.render(scene, camera);
+
+  // referencing my Twerk Lidar Robot project
+  // https://github.com/jdc-cunningham/twerk-lidar-robot/blob/dev/web-simulator/index.html
+  const loadFile = (filePath) => {
+    loader.load(
+      // resource URL
+      `${baseGlbPath}/${filePath}`,
+      // called when the resource is loaded
+      function ( gltf ) {
+
+        gltfs[filePath.replace('.glb', '')] = {
+          handle: gltf,
+        };
+
+        console.log(gltf);
+
+        scene.add(new THREE.AmbientLight(0xdddddd)); // controls brightness of colors
+        // gltf.scene.position.x=10
+        scene.add( gltf.scene );
+
+        gltf.animations; // Array<THREE.AnimationClip>
+        gltf.scene; // THREE.Group
+        gltf.scenes; // Array<THREE.Group>
+        gltf.cameras; // Array<THREE.Camera>
+        gltf.asset; // Object
+
+        renderer.render( scene, camera );
+        gltfs["basic-robot-model"].handle.scene.rotateX(degToRad(90)); // match blue as Z axis
+      },
+      // called while loading is progressing
+      function ( xhr ) {
+        console.log( ( xhr.loaded / xhr.total * 100 ) + '% loaded' );
+      },
+      // called when loading has errors
+      function ( error ) {
+        console.log( 'An error happened' );
+      }
+    );
+  }
+
+  loadFile(robotGlbFilename);
   animate();
 };
 
