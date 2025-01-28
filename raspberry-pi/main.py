20b1d91cae62ea6bb9de3f7b661175f5b8f5ee92
Jacob Cunningham <jcun4128@gmail.com>
2025-01-27 18:44:07 -0600

    just use separate files

--- raspberry-pi/main.py
+++ raspberry-pi/main.py
@@ -7,26 +7,15 @@ from navigation.navigation import Navigation
 from socket_server.socket_server import SocketServer
 from socket_client.socket_client import SocketClient
 
-import os
+web_ui_socket = SocketServer()
+vehicle_socket = SocketClient()
+light = Led()
+motion = Servo()
+narrow_angle_sensor = NarrowSensor()
+wide_angle_sensor = WideSensor()
+navigation = Navigation(motion, wide_angle_sensor, narrow_angle_sensor, vehicle_socket)
 
-# assumes Raspberry Pi/Linux
-if (os.name == "posix"):
-  web_ui_socket = SocketServer()
-  vehicle_socket = SocketClient()
-  light = Led()
-  motion = Servo()
-  narrow_angle_sensor = NarrowSensor()
-  wide_angle_sensor = WideSensor()
-  navigation = Navigation(motion, wide_angle_sensor, narrow_angle_sensor, vehicle_socket)
-
-  light.off()
-  motion.boot_center()
-  # navigation.scan_floor()
-  navigation.full_floor_scan()
-else:
-  '''
-  here you can run code/mock things without the hardware
-  if your non-raspberry-pi OS is linux this may cause a problem eg. be posix
-  '''
-  print("windows")
-
+light.off()
+motion.boot_center()
+# navigation.scan_floor()
+navigation.full_floor_scan()
---
+++ raspberry-pi/test.py
@@ -0,0 +1,1 @@
+print("ran")
